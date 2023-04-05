6493bea4eb19f202354794777f588650a8576413
Maciel Azevedo <maciel.az.neto@gmail.com>
2023-04-05 14:21:26 -0300

    Estrutura visual - Estrutura da home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,32 @@
+import Image from "next/image";
+import { HomeContainer, Product } from "../styles/pages/home";
+
+import camiseta1 from '../assets/Shirt/1.png'
+import camiseta2 from '../assets/Shirt/2.png'
+import camiseta3 from '../assets/Shirt/3.png'
+import camiseta4 from '../assets/Shirt/4.png'
+
+
 export default function Home() {
   return (
-    <h1>hello world</h1>
+    <HomeContainer>
+      <Product>
+        <Image src={camiseta1} width={520} height={420} alt="" />
+
+        <footer>
+          <strong>Camiseta x</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+
+      <Product>
+        <Image src={camiseta1} width={520} height={420} alt="" />
+
+        <footer>
+          <strong>Camiseta x</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+    </HomeContainer>
   )
 }
