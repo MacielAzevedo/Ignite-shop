4c213065c969473526c84bc7fe411d937cc68781
Maciel Azevedo <maciel.az.neto@gmail.com>
2023-04-06 10:04:09 -0300

    Produto & Checkout - Estrutura da página de produto

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,9 +1,31 @@
+import { ImageContainer, ProductContainer, ProductDetail } from "@/src/styles/pages/product"
+import Image from "next/image"
 import { useRouter } from "next/router"
 
 export default function Product() {
     const { query } = useRouter()
 
     return (
-        <h1>Product: {JSON.stringify(query)}</h1>
+        <ProductContainer>
+            <ImageContainer>
+
+            </ImageContainer>
+            <ProductDetail>
+                <h1>Camiseta X</h1>
+                <span>R$ 79,90</span>
+
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                    Perferendis facilis placeat quae laboriosam quibusdam nemo 
+                    non dolorem excepturi mollitia repudiandae adipisci a 
+                    suscipit impedit blanditiis reprehenderit, eligendi illum, 
+                    nobis minima!
+                </p>
+
+                <button>
+                    Comprar agora
+                </button>
+            </ProductDetail>
+        </ProductContainer>
     )
 }
