762ac6279146069513745030241e4aa6e0eb88bf
Maciel Azevedo <maciel.az.neto@gmail.com>
2023-04-07 15:11:03 -0300

    Produto & Checkout - Dados da compra na página de sucesso

--- src/pages/api/checkout.ts
+++ src/pages/api/checkout.ts
@@ -1,21 +1,23 @@
-import { stripe } from "@/src/lib/stripe";
-import { NextApiRequest, NextApiResponse } from "next";
+import { stripe } from '@/src/lib/stripe';
+import { NextApiRequest, NextApiResponse } from 'next';
 
-export default async function handler  (req:NextApiRequest, res:NextApiResponse) {
-  const {priceId} = req.body
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const { priceId } = req.body;
 
-  if(req.method !== 'POST') {
-    return res.status(405).json({error: 'Method not allowed.'})
+  if (req.method !== 'POST') {
+    return res.status(405).json({ error: 'Method not allowed.' });
   }
 
   if (!priceId) {
-    return res.status(400).json({error: 'Price not found.'})
+    return res.status(400).json({ error: 'Price not found.' });
   }
 
-  const successUrl = `${process.env.NEXT_URL}/sucess`
-  const canceUrl = `${process.env.NEXT_URL}/`
+  const successUrl = `${process.env.NEXT_URL}/sucess?session_id={CHECKOUT_SESSION_ID}`;
+  const canceUrl = `${process.env.NEXT_URL}/`;
 
-  
   const checkoutSession = await stripe.checkout.sessions.create({
     success_url: successUrl,
     cancel_url: canceUrl,
@@ -24,11 +26,11 @@ export default async function handler  (req:NextApiRequest, res:NextApiResponse)
       {
         price: priceId,
         quantity: 1,
-      }
+      },
     ],
-  })
+  });
 
   return res.status(201).json({
-    checkoutUrl: checkoutSession.url
-  })
+    checkoutUrl: checkoutSession.url,
+  });
 }
--- src/pages/sucess/index.tsx
+++ src/pages/sucess/index.tsx
@@ -1,23 +1,59 @@
+import { stripe } from '@/src/lib/stripe';
+import Image from 'next/image';
 import {
   Button,
   ImageContainer,
   SucessContainer,
 } from '@/src/styles/pages/sucess';
+import { GetServerSideProps } from 'next';
+import Stripe from 'stripe';
 
-export default function Sucess() {
+interface SucessProps {
+  customerName: string;
+  product: {
+    name: string;
+    imageUrl: string;
+  };
+}
+
+export default function Sucess({ customerName, product }: SucessProps) {
   return (
     <SucessContainer>
       <h1>Compra efetuada!</h1>
 
-      <ImageContainer></ImageContainer>
+      <ImageContainer>
+        <Image src={product.imageUrl} width={120} height={110} alt="" />
+      </ImageContainer>
 
       <p>
-        Uhuul <strong>Diego Fernandes</strong>, sua{' '}
-        <strong>Camiseta Beyond the Limits</strong> já está a caminho da sua
-        casa.
+        Uhuul <strong>{customerName}</strong>, sua{' '}
+        <strong>{product.name}</strong> já está a caminho da sua casa.
       </p>
 
       <Button href="/">Voltar ao catálogo</Button>
     </SucessContainer>
   );
 }
+
+export const getServerSideProps: GetServerSideProps = async ({ query }) => {
+  const sessionId = String(query.session_id);
+
+  const session = await stripe.checkout.sessions.retrieve(sessionId, {
+    expand: ['line_items', 'line_items.data.price.product'],
+  });
+
+  // @ts-ignore
+  const customerName = session.customer_details.name;
+  // @ts-ignore
+  const product = session.line_items.data[0].price.product as Stripe.Product;
+
+  return {
+    props: {
+      customerName,
+      product: {
+        name: product.name,
+        imageUrl: product.images[0],
+      },
+    },
+  };
+};
