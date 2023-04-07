a30237ff24644de2768d067ca0d7c18ce94b8f48
Maciel Azevedo <maciel.az.neto@gmail.com>
2023-04-07 13:50:24 -0300

    Produto & Checkout - Estrutura da página de  sucesso

--- src/pages/api/checkout.ts
+++ src/pages/api/checkout.ts
@@ -6,7 +6,6 @@ export default async function handler  (req:NextApiRequest, res:NextApiResponse)
 
   if(req.method !== 'POST') {
     return res.status(405).json({error: 'Method not allowed.'})
-
   }
 
   if (!priceId) {
--- src/pages/sucess/index.tsx
+++ src/pages/sucess/index.tsx
@@ -1,5 +1,23 @@
+import {
+  Button,
+  ImageContainer,
+  SucessContainer,
+} from '@/src/styles/pages/sucess';
+
 export default function Sucess() {
-    return (
-        <h1>Sucess</h1>
-    )
+  return (
+    <SucessContainer>
+      <h1>Compra efetuada!</h1>
+
+      <ImageContainer></ImageContainer>
+
+      <p>
+        Uhuul <strong>Diego Fernandes</strong>, sua{' '}
+        <strong>Camiseta Beyond the Limits</strong> já está a caminho da sua
+        casa.
+      </p>
+
+      <Button href="/">Voltar ao catálogo</Button>
+    </SucessContainer>
+  );
 }
